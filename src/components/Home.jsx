0837b1c23f09e7a890b799f19f8cd56677f5ad5b
lichaa824 <lisandrorubi@gmail.com>
2021-12-20 12:39:22 -0300

    Added title animation , background , and fix margin in shop

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,18 @@
-function Home(){
-    return(
-        <h3>HOMEPAGE</h3>
-    )
-};
+import "../styles/home.scss";
+import "../styles/titleanimation.scss";
+
+function Home() {
+  return (
+    <div className="home-page">
+      <section className="container">
+        <h1 className="title">
+          <span>Welcome!</span>
+          <span>to the</span>
+          <span>Gaming Shop</span>
+        </h1>
+      </section>
+    </div>
+  );
+}
 
 export default Home;
